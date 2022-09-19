1ab11fabe698df9ea1457bab5861847c5cca3eb6
vietnt2810 <viet.nt2810@gmail.com>
2022-09-19 10:09:14 +0700

    Refactor code for like-button 2

--- js/index.js
+++ js/index.js
@@ -3,21 +3,19 @@ $(document).ready(function () {
     var likeHolder = $(this);
     var textChild = likeHolder.children("span");
     var text = textChild.text();
-    var count = text.replace(/\D/g, "");
+    var counter = text.replace(/\D/g, "");
     var iconChild = likeHolder.children("i");
-    if (likeHolder.hasClass("notliked")) {
-      likeHolder.removeClass("notliked");
-      likeHolder.addClass("liked");
-      count++;
-      iconChild.removeClass("fa-regular");
-      iconChild.addClass("fa-solid");
-    } else if (likeHolder.hasClass("liked")) {
-      likeHolder.removeClass("liked");
-      likeHolder.addClass("notliked");
-      count--;
-      iconChild.removeClass("fa-solid");
-      iconChild.addClass("fa-regular");
-    }
-    textChild.text(`${count} likes`);
+    likeHolder.hasClass("notliked")
+      ? (likeHolder.removeClass("notliked"),
+        likeHolder.addClass("liked"),
+        counter++,
+        iconChild.removeClass("fa-regular"),
+        iconChild.addClass("fa-solid"))
+      : (likeHolder.removeClass("liked"),
+        likeHolder.addClass("notliked"),
+        counter--,
+        iconChild.removeClass("fa-solid"),
+        iconChild.addClass("fa-regular"));
+    textChild.text(`${counter} likes`);
   });
 });
